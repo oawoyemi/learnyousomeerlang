35a042f79e8019f8ce36f1342f1bc52c52e911dd
tundi <tundi@bulbview.com>
2015-10-05 09:22:24 +0100

    Updated kitchen.erl.

--- kitchen.erl
+++ kitchen.erl
@@ -32,4 +32,14 @@ fridge2(FoodList) ->
       ok
   end.
 
-  
+  store(Pid, Food) ->
+    Pid ! {self(), {store, Food}},
+    receive
+      {Pid, Msg} -> Msg
+    end.
+
+  take(Pid, Food) ->
+    Pid ! {self(), {take, Food}},
+    receive
+      {Pid, Msg} -> Msg
+    end.
